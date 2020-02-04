3296499e9dd02b385938dda19a4c0d6dca6f881d
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-02-05 01:40:36 +0300

    finish command tests

--- hw01-cli/test/commands/CommandTest.cpp
+++ hw01-cli/test/commands/CommandTest.cpp
@@ -3,8 +3,45 @@
 
 BOOST_AUTO_TEST_SUITE(CommandSuite)
 
-    BOOST_AUTO_TEST_CASE(testSmth) {
+    BOOST_AUTO_TEST_CASE(testAsStringEmptyCommand) {
+        Command command = Command(CommandName(""), CommandArguments());
+        BOOST_CHECK_EQUAL("", command.asString());
+    }
 
+    BOOST_AUTO_TEST_CASE(testAsStringEmptyArguments) {
+        Command command = Command(CommandName("cmd"), CommandArguments());
+        BOOST_CHECK_EQUAL("cmd", command.asString());
+    }
+
+    BOOST_AUTO_TEST_CASE(testAsStringEmptyName) {
+        Command command = Command(
+                CommandName(""),
+                CommandArguments(
+                        {
+                                Token(TokenType::LITERAL, "arg1"),
+                                Token(TokenType::SPACE, " "),
+                                Token(TokenType::DOLLAR, "$"),
+                                Token(TokenType::LITERAL, "var")
+                        }
+                )
+        );
+        BOOST_CHECK_EQUAL("arg1 $var", command.asString());
+    }
+
+    BOOST_AUTO_TEST_CASE(testAsString) {
+        Command command = Command(
+                CommandName("echo"),
+                CommandArguments(
+                        {
+                                Token(TokenType::SPACE, " "),
+                                Token(TokenType::LITERAL, "arg1"),
+                                Token(TokenType::SPACE, " "),
+                                Token(TokenType::DOLLAR, "$"),
+                                Token(TokenType::LITERAL, "var")
+                        }
+                )
+        );
+        BOOST_CHECK_EQUAL("echo arg1 $var", command.asString());
     }
 
 BOOST_AUTO_TEST_SUITE_END()
