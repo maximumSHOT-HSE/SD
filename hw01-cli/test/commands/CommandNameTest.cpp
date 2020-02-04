63a7780621eaf2e3735c75d10a20f9963e29f5fd
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-02-05 01:32:25 +0300

    add tests for command name and readme for commands

--- hw01-cli/test/commands/CommandNameTest.cpp
+++ hw01-cli/test/commands/CommandNameTest.cpp
@@ -3,8 +3,30 @@
 
 BOOST_AUTO_TEST_SUITE(CommandNameSuite)
 
-    BOOST_AUTO_TEST_CASE(testSmth) {
+    BOOST_AUTO_TEST_CASE(testGetNameEmptyName) {
+        BOOST_CHECK_EQUAL("", CommandName("").getName());
+    }
 
+    BOOST_AUTO_TEST_CASE(testGetName) {
+        BOOST_CHECK_EQUAL(
+                "\"qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+1234567890-=\"",
+                CommandName("\"qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+1234567890-=\"").getName()
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(testEqual2EmptyNames) {
+        BOOST_CHECK_EQUAL(true, CommandName("") == CommandName(""));
+    }
+
+    BOOST_AUTO_TEST_CASE(testEqualEmptyNameVsNonEmptyName) {
+        BOOST_CHECK_EQUAL(false, CommandName("") == CommandName("abc"));
+        BOOST_CHECK_EQUAL(false, CommandName("abc") == CommandName(""));
+    }
+
+    BOOST_AUTO_TEST_CASE(testEqual2NonEmptyNames) {
+        std::string name = "\"qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+1234567890-=\"";
+        BOOST_CHECK_EQUAL(false, CommandName("abc") == CommandName("xyz"));
+        BOOST_CHECK_EQUAL(true, CommandName(name) == CommandName(name));
     }
 
 BOOST_AUTO_TEST_SUITE_END()
