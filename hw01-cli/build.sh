231ec10bf7a970995da6e0fd12236fba5eeb8854
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-02-12 00:43:59 +0300

    fix azure yml

--- hw01-cli/build.sh
+++ hw01-cli/build.sh
@@ -2,7 +2,7 @@
 # Build a project in 'build' directory
 
 unset BOOST_ROOT
-sudo apt install -y libboost-atomic-dev libboost-thread-dev libboost-system-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-random-dev libboost-chrono-dev libboost-serialization-dev
+sudo apt install -y libboost-dev libboost-thread-dev libboost-system-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-random-dev libboost-chrono-dev libboost-serialization-dev
 
 export CC=gcc-8
 export CXX=g++-8
