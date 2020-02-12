5fabda6a80044c7ce8cad649b7485d755eeb3c6e
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-02-12 03:01:50 +0300

    add plugs for grep tests

---
+++ hw01-cli/test/executors/grep/GrepArgumentsTest.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+#include <executors/grep/GrepArguments.h>
+
+BOOST_AUTO_TEST_SUITE(GrepArgumentsSuite)
+
+    BOOST_AUTO_TEST_CASE(testSmth) {
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ hw01-cli/test/executors/grep/GrepExecutorTest.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+#include <executors/grep/GrepExecutor.h>
+
+BOOST_AUTO_TEST_SUITE(GrepExecutorSuite)
+
+    BOOST_AUTO_TEST_CASE(testSmth) {
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
